b95f15abf7c2146e837122d479b522c31ab37b2e
savad k <124030718+S4vad@users.noreply.github.com>
2024-12-19 23:12:24 +0530

    progress

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,12 +1,31 @@
+import { useRef ,useState} from "react";
 import Input from "../../Ui/Input";
 const MealItemForm = () => {
+
+  const [amountValid,setAmountValid] = useState(true);
+
+  const amountInputRef = useRef();
+
   const submitHandler = (event) => {
     event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value; //it always string
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountValid(false);
+      return; 
+    }
+    props.onAddToCart(enteredAmountNumber)
   };
   return (
     <form className="text-right" onSubmit={submitHandler}>
       <Input
-        ref
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount",
@@ -20,6 +39,7 @@ const MealItemForm = () => {
       <button className="cursor-pointer bg-[#8a2b06] border border-[#8a2b06] text-white py-1 px-8 rounded-full font-bold hover:bg-[#641e03] hover:border-[#641e03] active:bg-[#641e03] active:border-[#641e03]">
         + Add
       </button>
+      {!amountValid && <p> please enter a valid amount (1-5)</p>}
     </form>
   );
 };
--- src/components/Ui/Input.jsx
+++ src/components/Ui/Input.jsx
@@ -1,24 +1,26 @@
+import React from "react";
 import PropTypes from "prop-types";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className="flex items-center mb-2">
       <label htmlFor={props.input.id} className="font-bold mr-4">
         {props.label}
       </label>
       <input
+        ref={ref}
         {...props.input}
         className="w-12 rounded-md border border-gray-300 font-inherit pl-2"
       />
     </div>
   );
-};
+});
 
 // PropTypes validation
 Input.propTypes = {
   label: PropTypes.string.isRequired, // The label must be a string and is required
   input: PropTypes.shape({
-    id: PropTypes.string.isRequired,  // input id must be a string and required
+    id: PropTypes.string.isRequired, // input id must be a string and required
     type: PropTypes.string.isRequired, // input type must be a string and required
     value: PropTypes.string.isRequired, // input value must be a string and required
   }).isRequired,
